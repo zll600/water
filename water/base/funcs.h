11813b24a11052df1420868d94331edbbc1e3143
zll600 <3400692417@qq.com>
2022-06-19 23:46:41 +0800

    add utility functions

---
+++ water/base/funcs.h
@@ -0,0 +1,19 @@
+#ifndef __WATERT_BASE_FUNCS_H__
+#define __WATERT_BASE_FUNCS_H__
+
+#include <cstdint>
+#include <arpa/inet.h>
+
+namespace water {
+
+uint64_t hton64(uint64_t port) {
+    return (static_cast<uint64_t>(htonl(port)) << 32) | htonl(port >> 32);
+}
+
+uint64_t ntohl64(uint64_t port) {
+    return (static_cast<uint64_t>(ntohl(port)) << 32) | ntohl(port >> 32);
+}
+
+}   // namespace water
+
+#endif // __WATERT_BASE_FUNCS_H__
